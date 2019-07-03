3a41e40480d22c13057ff6fb04c2dbc99169f609
SirBilliamson <12832195+SirBilliamson@users.noreply.github.com>
2019-07-03 08:48:37 -0500

    new setup file

---
+++ setup-macos.sh
@@ -0,0 +1,274 @@
+#!/usr/bin/env bash
+
+main() {
+    # First things first, asking for sudo credentials
+    ask_for_sudo
+    # Installing Homebrew, the basis of anything and everything
+    install_homebrew
+    # Cloning Dotfiles repository for install_packages_with_brewfile to have access to Brewfile
+    clone_dotfiles_repo
+    # Installing all packages in Dotfiles repository's Brewfile
+    install_packages_with_brewfile
+    # Changing default shell to Fish
+    change_shell_to_fish
+    # Installing pip packages so that setup_symlinks can setup the symlinks
+    install_pip_packages
+    # Installing yarn packages
+    install_yarn_packages
+    # Mackup restore for settings
+    mackup_restore
+    # Setting up Vim
+    setup_vim
+    # Setting up tmux
+    setup_tmux
+}
+
+DOTFILES_REPO=~/personal/dotfiles
+
+function ask_for_sudo() {
+    info "Prompting for sudo password"
+    if sudo --validate; then
+        # Keep-alive
+        while true; do sudo --non-interactive true; \
+            sleep 10; kill -0 "$$" || exit; done 2>/dev/null &
+        success "Sudo password updated"
+    else
+        error "Sudo password update failed"
+        exit 1
+    fi
+}
+
+function install_homebrew() {
+    info "Installing Homebrew"
+    if hash brew 2>/dev/null; then
+        success "Homebrew already exists"
+    else
+url=https://raw.githubusercontent.com/Homebrew/install/master/install
+        if /usr/bin/ruby -e "$(curl -fsSL ${url})"; then
+            success "Homebrew installation succeeded"
+        else
+            error "Homebrew installation failed"
+            exit 1
+        fi
+    fi
+}
+
+function install_packages_with_brewfile() {
+    BREW_FILE_PATH="${DOTFILES_REPO}/brew/macOS.Brewfile"
+    info "Installing packages within ${BREW_FILE_PATH}"
+    if brew bundle check --file="$BREW_FILE_PATH" &> /dev/null; then
+        success "Brewfile's dependencies are already satisfied "
+    else
+        if brew bundle --file="$BREW_FILE_PATH"; then
+            success "Brewfile installation succeeded"
+        else
+            error "Brewfile installation failed"
+            exit 1
+        fi
+    fi
+}
+
+function change_shell_to_fish() {
+    info "Fish shell setup"
+    if grep --quiet fish <<< "$SHELL"; then
+        success "Fish shell already exists"
+    else
+        user=$(whoami)
+        substep "Adding Fish executable to /etc/shells"
+        if grep --fixed-strings --line-regexp --quiet \
+            "/usr/local/bin/fish" /etc/shells; then
+            substep "Fish executable already exists in /etc/shells"
+        else
+            if echo /usr/local/bin/fish | sudo tee -a /etc/shells > /dev/null;
+            then
+                substep "Fish executable successfully added to /etc/shells"
+            else
+                error "Failed to add Fish executable to /etc/shells"
+                exit 1
+            fi
+        fi
+        substep "Switching shell to Fish for \"${user}\""
+        if sudo chsh -s /usr/local/bin/fish "$user"; then
+            success "Fish shell successfully set for \"${user}\""
+        else
+            error "Please try setting Fish shell again"
+        fi
+    fi
+}
+
+function install_pip_packages() {
+    pip_packages=(powerline-status requests tmuxp virtualenv django mypy)
+    info "Installing pip packages \"${pip_packages[*]}\""
+
+    pip3_list_outcome=$(pip3 list)
+    for package_to_install in "${pip_packages[@]}"
+    do
+        if echo "$pip3_list_outcome" | \
+            grep --ignore-case "$package_to_install" &> /dev/null; then
+            substep "\"${package_to_install}\" already exists"
+        else
+            if pip3 install "$package_to_install"; then
+                substep "Package \"${package_to_install}\" installation succeeded"
+            else
+                error "Package \"${package_to_install}\" installation failed"
+                exit 1
+            fi
+        fi
+    done
+
+    success "pip packages successfully installed"
+}
+
+function install_yarn_packages() {
+    # Installing typescript for YouCompleteMe and prettier for Neoformat to auto-format files
+    # json for auto-formatting of json responses in terminal
+    # vmd for previewing markdown files
+    yarn_packages=(prettier typescript json vmd create-react-app gatsby-cli netlify-cli)
+    info "Installing yarn packages \"${yarn_packages[*]}\""
+
+    yarn_list_outcome=$(yarn global list)
+    for package_to_install in "${yarn_packages[@]}"
+    do
+        if echo "$yarn_list_outcome" | \
+            grep --ignore-case "$package_to_install" &> /dev/null; then
+            substep "\"${package_to_install}\" already exists"
+        else
+            if yarn global add "$package_to_install"; then
+                substep "Package \"${package_to_install}\" installation succeeded"
+            else
+                error "Package \"${package_to_install}\" installation failed"
+                exit 1
+            fi
+        fi
+    done
+
+    success "yarn packages successfully installed"
+}
+
+function clone_dotfiles_repo() {
+    info "Cloning dotfiles repository into ${DOTFILES_REPO}"
+    if test -e $DOTFILES_REPO; then
+        substep "${DOTFILES_REPO} already exists"
+        pull_latest $DOTFILES_REPO
+        success "Pull successful in ${DOTFILES_REPO} repository"
+    else
+        url=https://github.com/SirBilliamson/dotfiles.git
+        if git clone "$url" $DOTFILES_REPO && \
+           git -C $DOTFILES_REPO remote set-url origin git@github.com/SirBilliamson/dotfiles.git; then
+            success "Dotfiles repository cloned into ${DOTFILES_REPO}"
+        else
+            error "Dotfiles repository cloning failed"
+            exit 1
+        fi
+    fi
+}
+
+function pull_latest() {
+    substep "Pulling latest changes in ${1} repository"
+    if git -C $1 pull origin master &> /dev/null; then
+        return
+    else
+        error "Please pull latest changes in ${1} repository manually"
+    fi
+}
+
+function setup_vim() {
+    info "Setting up vim"
+    substep "Installing Vundle"
+    if test -e ~/.vim/bundle/Vundle.vim; then
+        substep "Vundle already exists"
+        pull_latest ~/.vim/bundle/Vundle.vim
+        substep "Pull successful in Vundle's repository"
+    else
+        url=https://github.com/VundleVim/Vundle.vim.git
+        if git clone "$url" ~/.vim/bundle/Vundle.vim; then
+            substep "Vundle installation succeeded"
+        else
+            error "Vundle installation failed"
+            exit 1
+        fi
+    fi
+    substep "Installing all plugins"
+    if vim +PluginInstall +qall 2> /dev/null; then
+        substep "Plugins installations succeeded"
+    else
+        error "Plugins installations failed"
+        exit 1
+    fi
+    success "vim successfully setup"
+}
+
+function setup_tmux() {
+    info "Setting up tmux"
+    substep "Installing tpm"
+    if test -e ~/.tmux/plugins/tpm; then
+        substep "tpm already exists"
+        pull_latest ~/.tmux/plugins/tpm
+        substep "Pull successful in tpm's repository"
+    else
+        url=https://github.com/tmux-plugins/tpm
+        if git clone "$url" ~/.tmux/plugins/tpm; then
+            substep "tpm installation succeeded"
+        else
+            error "tpm installation failed"
+            exit 1
+        fi
+    fi
+
+    substep "Installing all plugins"
+
+    # sourcing .tmux.conf is necessary for tpm
+    tmux source-file ~/.tmux.conf 2> /dev/null
+
+    if ~/.tmux/plugins/tpm/bin/./install_plugins &> /dev/null; then
+        substep "Plugins installations succeeded"
+    else
+        error "Plugins installations failed"
+        exit 1
+    fi
+    success "tmux successfully setup"
+}
+
+function mackup_restore {
+    mackup restore
+}
+
+function coloredEcho() {
+    local exp="$1";
+    local color="$2";
+    local arrow="$3";
+    if ! [[ $color =~ '^[0-9]$' ]] ; then
+       case $(echo $color | tr '[:upper:]' '[:lower:]') in
+        black) color=0 ;;
+        red) color=1 ;;
+        green) color=2 ;;
+        yellow) color=3 ;;
+        blue) color=4 ;;
+        magenta) color=5 ;;
+        cyan) color=6 ;;
+        white|*) color=7 ;; # white or invalid color
+       esac
+    fi
+    tput bold;
+    tput setaf "$color";
+    echo "$arrow $exp";
+    tput sgr0;
+}
+
+function info() {
+    coloredEcho "$1" blue "========>"
+}
+
+function substep() {
+    coloredEcho "$1" magenta "===="
+}
+
+function success() {
+    coloredEcho "$1" green "========>"
+}
+
+function error() {
+    coloredEcho "$1" red "========>"
+}
+
+main "$@"
--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/ bash
+#!/bin/bash
 
 main() {
 	# Ask for sudo
